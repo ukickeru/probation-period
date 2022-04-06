89fe68d84b0aae2a25e296479d7b4a2235a17d01
Pavel Nikolaev <p.nikolaev@mygento.ru>
2022-04-06 11:00:38 +0300

    Fixed entrypoint script, due to which it was not possible to restart the application container
    Signed-off-by: Pavel Nikolaev <p.nikolaev@mygento.ru>

--- app/start.sh
+++ app/start.sh
@@ -4,12 +4,6 @@
 /usr/sbin/sshd -D </dev/null &>/dev/null &
 
 # Run Symfony web server
-ps -C symfony
-
-if [[ -$? -ne 0 ]]
-then
-  >&1 echo 'Start Symfony server...'
-  symfony server:start --port=8080
-else
-  >&1 echo 'Symfony server is already running!'
-fi
+printf '\nStarting Symfony server...\n\n'
+symfony server:start -d --port=8080
+symfony server:log
